4d9904091760206247bdba48a00a5cb9d6cdf4b2
yumit <yumit.ismailov@inbestme.com>
2023-06-11 15:13:33 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const PORT = process.ENV.PORT || 8000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const { response } = require('express')
+
+const app = express()
+const newspapers = [
+    {
+        name: "thetimes",
+        address: "https://www.thetimes.co.uk/environment/climate-change",
+        base: ""
+    },
+    {
+        name: "theguardian",
+        address: "https://www.theguardian.com/environment/climate-crisis",
+        base: ""
+    },
+    {
+        name: "telegraph",
+        address: "https://www.telegraph.co.uk/climate-change",
+        base: "https://www.telegraph.co.uk"
+    },
+]
+const articles = []
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+         .then(response => {
+             const html = response.data
+             const $ = cheerio.load(html)
+
+             $('a:contains("climate")', html).each(function (){
+                const title = $(this).text()
+                const url = newspaper.base + $(this).attr('href')
+                articles.push({title, url, source: newspaper.name}) 
+            })
+         })
+})
+
+app.get('/', (req , res) => {
+    res.json("Welcome to my climate change news api")
+})
+
+app.get('/news', (req, res) => {
+    res.json(articles)
+})
+
+app.get('/news/:newspaperId', (req, res) => {
+    const newspaperId = req.params.newspaperId
+    const newspaper = newspapers.filter(newspaper => newspaper.name === newspaperId)[0]
+    axios.get(newspaper.address)
+         .then(response => {
+             const html = response.data
+             const $ = cheerio.load(html)
+             const specificArticles = []
+
+             $('a:contains("climate")', html).each(function (){
+                const title = $(this).text()
+                const url = newspaper.base + $(this).attr('href')
+                specificArticles.push({title, url, source: newspaperId}) 
+            })
+            res.json(specificArticles)
+        })
+        .catch(err => console.log(err))
+
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)) 
